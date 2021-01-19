a74ba02aa5c9132e8ba3ba3217da66f042dcf381
Sidharth <sidforjee@gmail.com>
2021-01-19 18:43:54 +0530

    changed to dark theme

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -4,8 +4,6 @@
 	export let sudoku;
 	import SudokuCell from "./SudokuCell.svelte";
 
-	var ansHashed = "231645"; // TODO: store hash
-
 	function changed(i, j, val){
 		sudoku[i][j] = val;
 	}
@@ -17,16 +15,23 @@
 			enabled[i].push(sudoku[i][j] == null);
 	}
 
-	function encrypt(str){
-		return str;
+	var ansHashed = "359c74260eeec18bfcccc039831d128d3d484a64b8a1dd1c53bc8d0a3ec89f0c";
+
+	async function encrypt(input){
+		const msgBuffer = new TextEncoder().encode(input);
+		const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+		const hashArray=Array.from(new Uint8Array(hashBuffer));
+		const hashHex=hashArray.map(b=>('00'+b.toString(16)).slice(-2)).join('');
+		return hashHex;
 	}
 
-	function checkAns(){
-		if(encrypt(document.getElementsByName("ans")[0].value) == ansHashed)
+	async function checkAns(){
+		if(await encrypt(document.getElementsByName("ans")[0].value) == ansHashed)
 			alert("Congrats");
 		else
 			alert("Try Again");
 	}
+
 </script>
 
 
@@ -37,11 +42,15 @@
 		<p>XXXXXX is the first row of the Sudoku</p>	
 	</div>
 
-	<table align="center">
+	<table cellpadding="0" cellspacing="0" align="center">
 		{#each {length: sudoku.length} as _, i}
 			<tr>
 				{#each {length: sudoku.length} as _,j}
-					<SudokuCell enabled={enabled[i][j]} changed={changed} index_i={i} index_j={j} num={sudoku[i][j]}/>
+					<SudokuCell 
+						enabled={enabled[i][j]} changed={changed} 
+						index_i={i} 
+						index_j={j} 
+						num={sudoku[i][j]}/>
 				{/each}
 			</tr>
 		{/each}
@@ -54,7 +63,11 @@
 </main>
 
 <style>
+	:global(body){
+		background-color: black;
+	}
 	main {
+		color: white;
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
@@ -63,15 +76,29 @@
 	}
 
 	#call-icon{
-		vertical-align: center!important;
-		padding: 0;
 		font-size: 38px;
 		color: green;
 	}
 
+	#call-icon:hover{
+		cursor: pointer;
+	}
+
+	#ans{
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
 	table{
-		border: 2px solid black;
-		border-collapse: collapse;
+		border: 3px solid rgb(1, 119, 255);
+		border-collapse:collapse;
+	}
+
+	input{
+		margin-top: 0;
+		margin-bottom: 0;
+		margin-right: 5px;
 	}
 
 	div{
--- svelte-app/src/SudokuCell.svelte
+++ svelte-app/src/SudokuCell.svelte
@@ -4,21 +4,42 @@
     export let index_j;
     export let changed;
     export let enabled;
+
+    function getStyle(){
+        var style = "";
+        if(index_i == 2){
+            style += "border-bottom: 3px solid rgb(1, 119, 255);";
+            console.log(index_i, index_j);
+        }
+        if(index_j == 2){
+            style += "border-right: 3px solid rgb(1, 119, 255);";
+            console.log(index_i, index_j);
+        }
+        return style;
+    }
 </script>
 
 <td>
-    <input type="text" maxlength="1" disabled={!enabled} value="{(num == null) ? "" : num}" on:change={(val) => changed(index_i, index_j, val.target.value)}/>
+    <input type="text" maxlength="1" disabled={!enabled} 
+        value="{(num == null) ? "" : num}" 
+        style={getStyle()}
+        on:change={(val) => changed(index_i, index_j, val.target.value)}/>
 </td>
 
 <style>
     td input:disabled{
-        color: white;
-        background-color: black;
+        background-color: aquamarine;
+        color: black;
+    }
+    td input:disabled:hover{
+        background-color:aquamarine;
     }
     input:focus{
         border: none;
     }
     td input{
+        background-color: black;
+        color: white;
         margin: 0;
         border: none;
         width: 50px;
@@ -26,7 +47,12 @@
         text-align: center;
         padding: 5px;
     }
+    td input:hover{
+        cursor: pointer;
+        background-color: azure;
+    }
     td{
-        border: 1px solid black;
+        margin: 0;
+        border: 1px solid rgb(1, 119, 255);
     }
 </style>
