3b2b200baf68f6a8324dffadbf4524e342c0a676
Sidharth <sidforjee@gmail.com>
2021-02-15 00:34:54 +0530

    added themes to identify, added button component

--- src/routes/Picross/Puzzle.svelte
+++ src/routes/Picross/Puzzle.svelte
@@ -1,11 +1,13 @@
 <script>
   export let nextPuzzle;
+  export let lastAns;
   import { onMount } from "svelte";
   import Grid from "./Grid.svelte";
   import {navigate} from "svelte-routing";
   import { solutionGrid } from "./utils";
   import { getNextUrl, verifyHash, verifyPreviousAns } from "../../common";
   import { picrossHashed } from "../../constants";
+import Button from "../../components/button.svelte";
   
   let grid = solutionGrid;
 
@@ -55,10 +57,7 @@
         placeholder="Whose logo is it?"
         name="answer"
       />
-      <button
-        class="px-5 py-2 m-2 border border-teal-800 text-white transition duration-500 ease select-none bg-teal-600 rounded-lg focus:shadow-outline hover:bg-teal-800 focus:outline-none focus:shadow-outline"
-        on:click={() => submit()}>Check</button
-      >
+      <Button handlerFunction={submit} text="Check"/>
     </div>
   </div>
 </main>
