e63b005d9254b6739fcee0edea3905b6962be89b
Sidharth <sidforjee@gmail.com>
2021-02-15 09:52:49 +0530

    moved tailwind to external file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   purge: ["./src/**/*.svelte"],
+  theme: {
+    extend: {
+      colors: {
+        "orange": "#ff7417e0",
+        "dark": "#38322f",
+        "light": "#f6f4f4"
+      }
+    }
+  }
 };
