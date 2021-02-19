0167b0bb6e5605acab01b7635b4c1fc795e8068f
Sidharth <sidforjee@gmail.com>
2021-02-19 22:22:23 +0530

    minor fix

--- src/routes/Bomb/App.svelte
+++ src/routes/Bomb/App.svelte
@@ -19,7 +19,8 @@
 
     let item = localStorage.getItem("timeLeft");
 
-    if (item == 0 || item === null) localStorage.setItem("timeLeft", timeLeft);
+    if (item == 0 || item === null) 
+      localStorage.setItem("timeLeft", timeLeft);
     else{
       timer = Number.parseInt(item);
       handleClick();
